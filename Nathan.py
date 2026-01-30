be595cf8802eb4f4e480e5d188cbe69374ffd3ec
Nathan Julien <nathan.julien@etu.minesparis.psl.eu>
2026-01-30 11:09:54 +0100

    Class Grass

---
+++ Nathan.py
@@ -0,0 +1,15 @@
+import random
+class Grass :
+    def __init__(self,existence,regeneration):
+        self.existence=existence
+        self.regeneration=regeneration
+
+    def update(self):
+        if self.existence==0 and self.regeneration!=-1: 
+            self.regeneration+=1
+        if self.regeneration==7:
+            self.existence=1
+        if self.existence==0 and self.regeneration==-1: #fais spawn de l'herbe aléatoirement
+            if random()<=0.05 : 
+                self.existence=1 and self.regeneration=0
+
