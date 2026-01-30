ac2e16abb1088af7956eb81c9420aa1b9509ab8b
Pauline Normant <pauline.normant@etu.minesparis.psl.eu>
2026-01-30 12:48:22 +0100

    Melina update

--- Pauline.py
+++ Pauline.py
@@ -69,24 +69,28 @@ class Sheep:
                 self.energy += 15
                 dico[key][0].existence = 0
                 
-    def Reproduction(self,dico,key,l_free,dic_new):
+    def Move(self,dico,dic_new,key, l_free):
+        animal = dico[key][1]
+        # Regarde les voisins pour voir s'il y a de l'herbe"""
+        l_grass = []
         x, y = key
-        l_reprod = []
-        if (x+1,y) in dico and dico[(x+1,y)][1] and dico[(x+1,y)][1].type == 'mouton':
-            l_reprod.append((x+1,y))
-        elif (x-1,y) in dico and dico[(x-1,y)][1] and dico[(x-1,y)][1].type == 'mouton':
-            l_reprod.append((x-1,y))
-        elif (x,y+1) in dico and dico[(x,y+1)][1] and dico[(x,y+1)][1].type == 'mouton':
-            l_reprod.append((x,y+1))
-        elif (x,y-1) in dico and dico[(x,y-1)][1] and dico[(x,y-1)][1].type == 'mouton':
-            l_reprod.append((x,y-1))
-                    
-        if len(l_reprod) > 0:
-            self.energy -= 20
-            new_pos = random.choice(l_reprod)
-            new_sheep = Sheep('mouton', 0, 20)
-            pos_baby = random.choice(l_free)
-            dic_new[pos_baby] = (dico[pos_baby][0], new_sheep)
+        if (x+1,y) in dico and dico[(x+1,y)][0].existence == 1:
+            l_grass.append((x+1,y))
+        if (x-1,y) in dico and dico[(x-1,y)][0].existence == 1:
+            l_grass.append((x-1,y))
+        if (x,y+1) in dico and dico[(x,y+1)][0].existence == 1:
+            l_grass.append((x,y+1))
+        if (x,y-1) in dico and dico[(x,y-1)][0].existence == 1:
+            l_grass.append((x,y-1))
+                
+        if len(l_grass) > 0:
+            new_pos = random.choice(l_grass)
+        elif len(l_grass) == 0 and len(l_free) > 0:
+            # Déplace le mouton
+            new_pos = random.choice(l_free) 
+            
+        dic_new[new_pos] = (dico[new_pos][0], animal)
+        dic_new[key] = (dico[key][0], None)
             
     def Move(self,dico,dic_new,key, l_free):
         # Regarde les voisins pour voir s'il y a de l'herbe"""
