8184d115cc6f5d499c6ff5a156e9c848c9565750
Mélina Henry <melina.henry@etu.minesparis.psl.eu>
2026-01-30 12:24:51 +0100

    update sur sheep

--- Melina.py
+++ Melina.py
@@ -21,14 +21,16 @@ class Sheep():
             animal.Mort(dico,key)
             animal.OnGrass(dico,key)
                 
-                # Cherche les cases libres autour 
-                l_free = [] # CASES LIBRES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                for elem in l_move:
-                    if dico[elem][1] is None:
-                        l_free.append(elem)
-                
-            animal.Reproduction(dico,key)
-            animal.Move(dico,dic_new,key)
+            # Cherche les cases libres autour 
+            l_free = [] 
+            for elem in l_move:
+                if dico[elem][1] is None:
+                    l_free.append(elem)
+            
+            if animal.energy > 50:
+                animal.Reproduction(dico,key)
+            
+            animal.Move(dico,dic_new,key, l_free)
                                   
         dico[key] = dic_new[key]
                
@@ -45,8 +47,8 @@ class Sheep():
                 dico[key][0].existence = 0
                 
     def Reproduction(self,dico,key):
-        if animal.energy > 50:
-                l_reprod = []
+        x, y = key
+        l_reprod = []
         if (x+1,y) in dico and dico[(x+1,y)][1] and dico[(x+1,y)][1].type == 'mouton':
             l_reprod.append((x+1,y))
         elif (x-1,y) in dico and dico[(x-1,y)][1] and dico[(x-1,y)][1].type == 'mouton':
@@ -57,14 +59,14 @@ class Sheep():
             l_reprod.append((x,y-1))
                     
         if len(l_reprod) > 0:
-            animal.energy -= 20
+            self.energy -= 20
             new_pos = rd.choice(l_reprod)
             new_sheep = Sheep('mouton', 0, 20)
             
-    def Move(self,dico,dic_new,key):
+    def Move(self,dico,dic_new,key, l_free):
         # Regarde les voisins pour voir s'il y a de l'herbe"""
-            l_grass = []
-
+        l_grass = []
+        x, y = key
         if (x+1,y) in dico and dico[(x+1,y)][0].existence == 1:
             l_grass.append((x+1,y))
         if (x-1,y) in dico and dico[(x-1,y)][0].existence == 1:
@@ -78,9 +80,9 @@ class Sheep():
             new_pos = rd.choice(l_grass)
         if len(l_grass) == 0:
             # Déplace le mouton
-            new_pos = rd.choice(l_move) # VÉRIFIER QUEE LA CASE EST LIBRE ET DANS LE CADRE!!!!!!!!!!!!!!!!!!!!
+            new_pos = rd.choice(l_free) # VÉRIFIER QUEE LA CASE EST LIBRE ET DANS LE CADRE!!!!!!!!!!!!!!!!!!!!
             
-        dic_new[new_pos] = (dico[new_pos][0], animal) # RETIRER L'ANIMAL DE SA CASE ACTUELLE !!!!!!!!!!!!!!!!!!!
+        dic_new[new_pos] = (dico[new_pos][0], self) # RETIRER L'ANIMAL DE SA CASE ACTUELLE !!!!!!!!!!!!!!!!!!!
         dic_new[key] = (dico[key][0], None)
         
     
