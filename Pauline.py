42385bec142a024e9ff4fb5a640a724436f4f959
Pauline Normant <pauline.normant@etu.minesparis.psl.eu>
2026-01-30 14:22:25 +0100

    wolf can move

--- Pauline.py
+++ Pauline.py
@@ -117,7 +117,6 @@ class Wolf:
             l_free=[]
             animal = dico[key][1]
             herbe = dico[key][0]
-            Ate_sheep, key_ate_sheep = self.OnSheep()
             if (x+1,y) in dico and dico[(x+1,y)][1] is None:
                 l_free.append((x+1,y))
             if (x-1,y) in dico and dico[(x-1,y)][1] is None:
@@ -133,12 +132,12 @@ class Wolf:
                 animal.energy -=1
                 
                 animal.Mort(dico,key)
-                animal.OnSheep(dico,key)
+                animal.MangeAdjSheep(dico,key)
             
-                if animal.energy > 50:
+                if animal.energy > 80:
                     animal.Reproduction(dico,key,l_free)
             
-                animal.Move(dico,key, l_free,Ate_sheep,key_ate_sheep)
+                animal.Move(dico,key, l_free)
         #dico = dic_new.copy()                  
         
         return dico
@@ -150,28 +149,36 @@ class Wolf:
             dico[key] = (dico[key][0], None)
                     
     
-    def OnSheep(self,dico,key):
-        Ate_sheep = False 
-        key_ate_sheep = None
-        if dico[key][1].type == "mouton":
-                self.energy += 15
-                dico[key][0].existence = 0
-        return Ate_sheep, key_ate_sheep
+    def MangeAdjSheep(self,dico,key):
+        l_adj=[]
+        x,y=key
+        Ate_sheep=False
+        key_ate_sheep=None
+        for d in [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]:
+                if d in dico and dico[d][1]!= None and dico[d][1].type=="mouton":
+                    l_adj.append(d)
+        if l_adj!=[]:
+            key_ate_sheep=random.choice(l_adj)
+            Ate_sheep=True
+            self.energy += 30
+            dico[key_ate_sheep] = (dico[key][0],None)
+        return (Ate_sheep,key_ate_sheep)
                 
     def Reproduction(self,dico,key,l_free):
         if self.energy > 80 and l_free != []:
             self.energy -=20
-            new_wolf = Sheep('loup', 0, 40)
+            new_wolf = Wolf('loup', 0, 40)
             pos_baby = random.choice(l_free)
             dico[pos_baby] = (dico[pos_baby][0], new_wolf)
         
             
-    def Move(self,dico,key, l_free,Ate_sheep,key_ate_sheep):
+    def Move(self,dico,key, l_free):
+        Ate_sheep, key_ate_sheep = self.MangeAdjSheep(dico,key)
         animal = dico[key][1]
         new_pos=key
         if Ate_sheep :
             new_pos = key_ate_sheep
-        else : 
+        elif len(l_free)>0 : 
             new_pos = random.choice(l_free)
         if new_pos != key : 
             dico[new_pos] = (dico[new_pos][0], animal)
@@ -199,6 +206,7 @@ class Grid :
         self.draw()
         self.update_grass()
         self.update_sheep()
+        self.update_wolf()
         
     def update_grass(self):
         for x in range(GRID_SIZE):
@@ -207,6 +215,9 @@ class Grid :
     
     def update_sheep(self):
         Sheep.update(self,self.grille)
+    
+    def update_wolf(self):
+        Wolf.update(self,self.grille)
 
     
     def draw(self):
