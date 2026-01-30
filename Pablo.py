82c928c49cc21ae8831f3748b2b906095d79b3fe
Pablo Thoumyre--Rivero Campoy <pablo.thoumyre--rivero_campoy@etu.minesparis.psl.eu>
2026-01-30 11:32:10 +0100

    Affichage loup+moutons + algo dico remanié

--- Pablo.py
+++ Pablo.py
@@ -14,11 +14,10 @@ INITIAL_WOLVES = 10
 INITIAL_GLASS_COVERAGE  = 0.3 #30% de la surface est couverte par de l'herbe 
 
 class Grid : 
-
     def __init__(self):
         pyxel.init(GRID_SIZE*SIDE,GRID_SIZE*SIDE,title = "Ecosystème")
-        self.grille = self.grid_initiale()
         pyxel.load("Dessins.pyxres")
+        self.grille = self.grid_initiale()
         pyxel.run(self.update,self.draw)
         
     
@@ -30,12 +29,11 @@ class Grid :
     def draw(self):
         pyxel.cls(0)
         self.draw_grille()
-        
 
 
     
     def grid_initiale(self):
-        grille = {(x,y):[0,-1,(None,None,None)] for x in range(GRID_SIZE) for y in range(GRID_SIZE)} 
+        grille = {(x,y):[0,-1,'',None,None] for x in range(GRID_SIZE) for y in range(GRID_SIZE)} 
         #(position):[0 si rien, 1 si herbe; age herbe (-1 si jamais eu herbe);("mouton ou loup" ou None;age mouton/age loup (ou None); energy)]
         n = 0
         m = 0
@@ -48,14 +46,15 @@ class Grid :
                 n+=1
         while m < INITIAL_SHEEP :
             i,j = random.randint(0,GRID_SIZE-1),random.randint(0,GRID_SIZE-1)
-            if grille[(i,j)][2][0]== None:
-                grille[(i,j)][2][0] == "mouton"
+            if grille[(i,j)][2]=='':
+                grille[(i,j)][2] = "mouton"
                 m+=1
         while l < INITIAL_WOLVES : 
             i,j = random.randint(0,GRID_SIZE-1),random.randint(0,GRID_SIZE-1)
-            if grille[(i,j)][2][0]== None:
-                grille[(i,j)][2][0] == "loup"
+            if grille[(i,j)][2]=='':
+                grille[(i,j)][2] = "loup"
                 l+=1
+        print(grille)
         return grille
     
     def draw_grille(self):
@@ -64,13 +63,15 @@ class Grid :
                 if self.grille[(x,y)][0] == 1 :
                     color = 11
                     pyxel.rect(x*SIDE, y*SIDE, SIDE, SIDE, color)
-                else : 
+                elif self.grille[(x,y)][0]==0: 
                     color = 4
                     pyxel.rect(x*SIDE, y*SIDE, SIDE, SIDE, color)
-                if self.grille[(x,y)][2][0]=="mouton":
+                if self.grille[(x,y)][2]=="mouton":
                     pyxel.blt(x*SIDE,y*SIDE,0,0,0,SIDE,SIDE, colkey=BLACK)
-                if self.grille[(x,y)][2][0]=="loup":
+                elif self.grille[(x,y)][2]=="loup":
                     pyxel.blt(x*SIDE,y*SIDE,0,0,SIDE,SIDE,SIDE, colkey=WHITE)
+        pyxel.blt(0,0,0,0,0,SIDE,SIDE,colkey=BLACK)
+                
 
 if __name__ == "__main__":
     Grid()
